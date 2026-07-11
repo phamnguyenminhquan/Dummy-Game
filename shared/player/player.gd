2a8b5d4e12412ea6c39d841ae5cc6f4fed03c397
Uy <uy.huynh.cit24@eiu.edu.vn>
2026-07-11 21:47:19 +0700

    Fix player movement and add collision

--- shared/player/player.gd
+++ shared/player/player.gd
@@ -9,13 +9,13 @@ func _ready():
 func _process(delta):
 	velocity = Vector2.ZERO # The player's movement vector
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		velocity.x += 3
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		velocity.x -= 3
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		velocity.y += 3
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		velocity.y -= 3
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -24,8 +24,7 @@ func _process(delta):
 		$AnimatedSprite2D.stop()
 	
 	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size) # Prevent from leaving the screen
-	
+	move_and_slide()
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
